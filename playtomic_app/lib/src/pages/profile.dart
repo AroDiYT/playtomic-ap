cff4173f0e00a1e3c4abbe75243b35c574eb33eb
KoenVA <koenvanaken1999@gmail.com>
2024-04-24 16:26:22 +0200

    Add text indication of signed in user

--- playtomic_app/lib/src/pages/profile.dart
+++ playtomic_app/lib/src/pages/profile.dart
@@ -1,15 +1,29 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class Profile extends StatefulWidget {
-  const Profile({ super.key });
+  const Profile({super.key});
 
   @override
   _ProfileState createState() => _ProfileState();
 }
 
 class _ProfileState extends State<Profile> {
+  final user = FirebaseAuth.instance.currentUser!;
+
   @override
   Widget build(BuildContext context) {
-    return const Text("Profile Page");
+    return Column(
+      children: [
+        const Text("Profile Page"),
+        Text("signed in as ${user.email!}"),
+        MaterialButton(
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+            },
+            color: Colors.grey,
+            child: const Text("Sign Out"))
+      ],
+    );
   }
 }
