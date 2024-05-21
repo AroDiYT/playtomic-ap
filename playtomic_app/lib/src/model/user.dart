6b6b60e4cf839c01c9fc5a8ff068158348a38c4a
KoenVA <koenvanaken1999@gmail.com>
2024-05-21 13:39:59 +0200

    Update AppUser constructor

--- playtomic_app/lib/src/model/user.dart
+++ playtomic_app/lib/src/model/user.dart
@@ -1,33 +1,42 @@
-import 'dart:collection';
-
 class AppUser {
   // if you add a field, update the toMap() method
   String email;
   String name;
   String tel;
-  bool hasPremium = false;
-  Map<String, int> preferences = {
-    "favTime": -1,
-    "hand": -1,
-    "position": -1,
-    "type": -1,
-  };
-  int gender = -1;
-  DateTime dateOfBirth = DateTime(0);
-  String bio = "";
-  String location = "";
-  LinkedHashMap<String, bool> interests = LinkedHashMap.from({
-    "Ontdek de gemeenschap": false,
-    "Concurreren met anderen": false,
-    "Speel met vrienden": false,
-    "Mijn speelniveau kennen": false,
-    "Mijn vooruitgang bijhouden": false,
-    "Reserveer een baan": false,
-    "Zoek mensen om mee te spelen": false,
-  });
-  String avatarPic = "";
+  bool hasPremium;
+  Map<String, int> preferences;
+  int gender;
+  String dateOfBirth;
+  String bio;
+  Map<String, double> location;
+  Map<String, bool> interests;
+  String avatarPic;
 
-  AppUser({required this.email, required this.name, required this.tel});
+  AppUser(
+      {required this.email,
+      required this.name,
+      required this.tel,
+      this.hasPremium = false,
+      this.preferences = const <String, int>{
+        "favTime": -1,
+        "hand": -1,
+        "position": -1,
+        "type": -1,
+      },
+      this.gender = -1,
+      this.dateOfBirth = "",
+      this.bio = "",
+      this.location = const {"lat": 0, "lng": 0},
+      this.interests = const <String, bool>{
+        "Ontdek de gemeenschap": false,
+        "Concurreren met anderen": false,
+        "Speel met vrienden": false,
+        "Mijn speelniveau kennen": false,
+        "Mijn vooruitgang bijhouden": false,
+        "Reserveer een baan": false,
+        "Zoek mensen om mee te spelen": false,
+      },
+      this.avatarPic = ""});
 
   Map<String, dynamic> toMap() {
     return {
